7eda182b5945eb3e802b166a99150d2ba56bb96c
cbhowmick <chandrima.birati@gmail.com>
2017-07-26 17:04:43 -0500

    Add files via upload

---
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Admin admin1 = new Admin(new FirstLogger());
+            Admin admin2 = new Admin(new SeccondLogger());
+        }
+    }
+}
---
+++ DependencyInjection/DependencyInjection/SeccondLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class SeccondLogger : ILogger
+    {
+        public void WriteLogMessage(string message)
+        {
+            Console.WriteLine("I am Chandrima:\n", message);
+        }
+    }
+}
